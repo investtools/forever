a8820b719227f4d639069a65fd638227318e0ec4
Christopher Fricke <cfricke@gisinc.com>
2014-05-05 14:35:39 -0400

    update version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "david@trabian.com"
 license          "MIT"
 description      "Chef cookbook for running node apps using forever."
 long_description "See README.md"
-version          "0.2.0"
+version          "0.2.1"
 
 supports "redhat"
 supports "centos"
