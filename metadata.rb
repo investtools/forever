54678ee1e81a9b279658b52b7a5db8783dc33255
Christopher Fricke <cfricke@gisinc.com>
2014-05-15 10:06:06 -0400

    escape tildes in stop call
    use PID variable for status

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cfricke@gisinc.com"
 license          "MIT"
 description      "Chef cookbook for running node apps using forever on ubuntu. For of david trabian's chef-forever."
 long_description "See README.md"
-version          "0.2.10"
+version          "0.2.11"
 
 supports "redhat"
 supports "centos"
