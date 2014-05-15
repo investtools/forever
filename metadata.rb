19c0ca71ab899b60800603461382bd1584df93df
Christopher Fricke <cfricke@gisinc.com>
2014-05-15 09:23:25 -0400

    Everything needs to run as the specified user!  Even forever lists!
    Updated status to only return results for that process.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cfricke@gisinc.com"
 license          "MIT"
 description      "Chef cookbook for running node apps using forever on ubuntu. For of david trabian's chef-forever."
 long_description "See README.md"
-version          "0.2.8"
+version          "0.2.9"
 
 supports "redhat"
 supports "centos"
